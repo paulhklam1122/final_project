d810c1c021dd6123cdee7f843562daf5f8577906
Paul Lam <paulhklam1122@gmail.com>
2016-08-16 13:16:29 -0700

    css precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "final_project_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "savvy_saver_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Ignore bad email addresses and do not raise email delivery errors.
