95d187f0a0d646279940483b612630db6523cc40
Paul Lam <paulhklam1122@gmail.com>
2016-08-16 18:04:02 -0700

    bootstrap added to app js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.infinite-pages
+//= require bootstrap
 //= require bootstrap-sprockets
 //= require underscore
 //= require gmaps/google
