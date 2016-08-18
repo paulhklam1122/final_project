584720609f25e6d8bbc581b169cd11d746719f00
Paul Lam <paulhklam1122@gmail.com>
2016-08-17 17:24:05 -0700

    redirect page fixed

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,12 +3,12 @@ class CallbacksController < ApplicationController
     google_data = request.env['omniauth.auth']
     user = User.find_or_create_from_google google_data
     sign_in(user)
-    redirect_to root_path, notice: "Thanks for signing in with Google!"
+    redirect_to user_path(current_user), notice: "Thanks for signing in with Google!"
   end
   def facebook
     facebook_data = request.env['omniauth.auth']
     user = User.find_or_create_from_facebook facebook_data
     sign_in(user)
-    redirect_to root_path, notice: "Thanks for signing in with Facebook!"
+    redirect_to user_path(current_user), notice: "Thanks for signing in with Facebook!"
   end
 end
